260ca886a2ba04e58cc1dcf2e404cd9c06628015
Siddhant Shirodkar <siddhant19shirodkar12@gmail.com>
2024-07-07 05:35:23 -0400

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'S:/NEW/
 
 from exception import CustomException
 from logger import logging
+from components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -49,4 +51,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
